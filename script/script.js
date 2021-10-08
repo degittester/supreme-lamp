1ad27cc5912c7f8d57374c07461ee49ee1edb1a3
Nitesh Kumar <76529189+Nktech-Official@users.noreply.github.com>
2021-10-08 12:05:47 +0530

    added images to explain the steps

---
+++ script/script.js
@@ -0,0 +1,45 @@
+document.getElementById("step1").addEventListener("mouseover",function(){
+    document.getElementById("step1-image").style.display="block";
+})
+document.getElementById("step1").addEventListener("mouseout",function(){
+    document.getElementById("step1-image").style.display="none";
+})
+// step2
+document.getElementById("step2").addEventListener("mouseover",function(){
+    document.getElementById("step2-image").style.display="block";
+})
+document.getElementById("step2").addEventListener("mouseout",function(){
+    document.getElementById("step2-image").style.display="none";
+})
+
+// Step3
+document.getElementById("step3").addEventListener("mouseover",function(){
+    document.getElementById("step3-image").style.display="block";
+})
+document.getElementById("step3").addEventListener("mouseout",function(){
+    document.getElementById("step3-image").style.display="none";
+})
+
+// Step4
+document.getElementById("step4").addEventListener("mouseover",function(){
+    document.getElementById("step4-image").style.display="block";
+})
+document.getElementById("step4").addEventListener("mouseout",function(){
+    document.getElementById("step4-image").style.display="none";
+})
+
+// Step5
+document.getElementById("step5").addEventListener("mouseover",function(){
+    document.getElementById("step5-image").style.display="block";
+})
+document.getElementById("step5").addEventListener("mouseout",function(){
+    document.getElementById("step5-image").style.display="none";
+})
+
+// Step6
+document.getElementById("step6").addEventListener("mouseover",function(){
+    document.getElementById("step6-image").style.display="block";
+})
+document.getElementById("step6").addEventListener("mouseout",function(){
+    document.getElementById("step6-image").style.display="none";
+})
