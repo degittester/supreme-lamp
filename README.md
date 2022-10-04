7a6471bc3c8fb306e32de19ea6ecf4396e31fe68
Soumyajoy Das <70829126+soumyajoy01@users.noreply.github.com>
2022-10-04 07:44:38 +0530

    REVERB

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # supreme-lamp
 
-This is a testing Webpage feel free to Contribute to this project. Thank You!
+This is a testing Webpage feel free to Contribute to this project. Thank You! We are BACK AGAIN!
 
 # First Contributions
 
--- script/slideshow.js
+++ script/slideshow.js
@@ -1,13 +1,13 @@
 var k;
-var isauto=true;
-const autocarousel=()=>{
+var isauto = true;
+const autocarousel = () => {
 
-    var cnt=0;
-    var y=document.getElementsByClassName("slide");
-    var x=document.getElementsByClassName("text");
-    for(cnt=0;cnt<y.length;cnt++){
-        y[cnt].style.display="none";
-        x[cnt].style.display="none";
+    var cnt = 0;
+    var y = document.getElementsByClassName("slide");
+    var x = document.getElementsByClassName("text");
+    for (cnt = 0; cnt < y.length; cnt++) {
+        y[cnt].style.display = "none";
+        x[cnt].style.display = "none";
     }
     k++;
     /**
@@ -15,26 +15,27 @@ const autocarousel=()=>{
      */
     if (k > y.length) {
         k = 1
-    }    
+    }
 
-y[k-1].style.display="block";
-x[k-1].style.display="block";
-var getbutton =document.getElementsByClassName("slbutton");
-for(var p=0;p<getbutton.length;p++){
-    getbutton[p].className=getbutton[p].className.replace("active","");
-}
+    y[k - 1].style.display = "block";
+    x[k - 1].style.display = "block";
+    var getbutton = document.getElementsByClassName("slbutton");
+    for (var p = 0; p < getbutton.length; p++) {
+        getbutton[p].className = getbutton[p].className.replace("active", "");
+    }
 
-getbutton[k-1].className +="active";
-if(isauto===true){
-    setTimeout(autocarousel,6000);
-    /**
-     * Calling autocarousel function to make the next slide active after 6 seconds
-     */
+    getbutton[k - 1].className += "active";
+    if (isauto === true) {
+        setTimeout(autocarousel, 6000);
+        /**
+         * Calling autocarousel function to make the next slide active after 6 seconds
+         */
+    }
 }
-}
-window.addEventListener('load',firstauto)
-function firstauto(){
-    k=0;
+window.addEventListener('load', firstauto)
+
+function firstauto() {
+    k = 0;
     autocarousel();
 }
 /**
@@ -42,11 +43,11 @@ function firstauto(){
  * in order to stop the previous asynchronous timeout operation
  * Then resume the autocarousel, this time from the current index
  */
-const clickbutton=(e)=>{
-    isauto=false;
-    k=e-1;
-   setTimeout(()=>{
-       isauto=true;
-    autocarousel()
-   },6000) 
+const clickbutton = (e) => {
+    isauto = false;
+    k = e - 1;
+    setTimeout(() => {
+        isauto = true;
+        autocarousel()
+    }, 6000)
 }
